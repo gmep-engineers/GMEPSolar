a78011984ea877ab2f61b82ab8873f9b50fcfec0
grenrock <stevengrenrock@live.com>
2025-04-16 10:41:01 -0700

    combine to one MakeSingleLine function

--- GMEPSolar/PowerStationUserControl.cs
+++ GMEPSolar/PowerStationUserControl.cs
@@ -57,156 +57,9 @@ namespace GMEPSolar
       return PowerStation.Lots.FindAll(l => l.Action != UpdateAction.Delete);
     }
 
-    private void MakeSingleLine45kw(Point3d startingPoint)
+    private void MakeSingleLine(Point3d startingPoint)
     {
-      List<Lot> ActiveLots = GetActiveLots();
-      Point3d panelPoint = new Point3d(
-        startingPoint.X + 4.0365,
-        startingPoint.Y
-          - 6.3512
-          - (PowerStation.NumBatts > 4 ? 0.7026 * (PowerStation.NumBatts - 4) : 0),
-        0
-      );
-      if (ActiveLots.Count > 1)
-      {
-        CadObjectFunctions.MakeBlock(panelPoint, "IBEC 200A PANEL");
-        Point3d upperLeft = new Point3d(panelPoint.X - 0.35, panelPoint.Y, 0);
-        Point3d upperRight = new Point3d(panelPoint.X + 0.35, panelPoint.Y, 0);
-        Point3d lowerLeft = new Point3d(
-          panelPoint.X - 0.35,
-          panelPoint.Y - 1.1282 - ((ActiveLots.Count - 2) * 0.2934),
-          0
-        );
-        Point3d lowerRight = new Point3d(
-          panelPoint.X + 0.35,
-          panelPoint.Y - 1.1282 - ((ActiveLots.Count - 2) * 0.2934),
-          0
-        );
-        CadObjectFunctions.MakeLine(upperLeft, upperRight, "E-SYM1");
-        CadObjectFunctions.MakeLine(upperLeft, lowerLeft, "E-SYM1");
-        CadObjectFunctions.MakeLine(lowerLeft, lowerRight, "E-SYM1");
-        CadObjectFunctions.MakeLine(upperRight, lowerRight, "E-SYM1");
-        Point3d feederStart = new Point3d(
-          panelPoint.X,
-          panelPoint.Y
-            + 1.7885
-            + (PowerStation.NumBatts > 5 ? 0.7026 * (PowerStation.NumBatts - 5) : 0),
-          0
-        );
-        CadObjectFunctions.MakeLine(feederStart, panelPoint);
-        CadObjectFunctions.MakeText(
-          new Point3d(panelPoint.X - 0.0625, panelPoint.Y + 0.5, 0),
-          "3WG200",
-          true
-        );
-      }
-      else
-      {
-        Point3d point1 = new Point3d(startingPoint.X + 4.0355, startingPoint.Y - 5.2653, 0);
-        Point3d point2 = new Point3d(
-          point1.X,
-          point1.Y - 1.7 - (PowerStation.NumBatts > 4 ? 0.7026 * (PowerStation.NumBatts - 4) : 0),
-          0
-        );
-        Point3d point3 = new Point3d(point2.X + 4.0490, point2.Y, 0);
-        Point3d point4 = new Point3d(startingPoint.X + 8.0855, startingPoint.Y - 4.2534, 0);
-        CadObjectFunctions.MakeLine(point1, point2);
-        CadObjectFunctions.MakeLine(point2, point3);
-        CadObjectFunctions.MakeLine(point3, point4);
-      }
-
-      List<Point3d> panelBreakerPoints = new List<Point3d>();
-      List<Point3d> meterComboPoints = new List<Point3d>();
-      double panelBreakerPointYOffset = 0;
-      double meterComboPointXOffset = 0;
-      for (int i = 0; i < ActiveLots.Count; i++)
-      {
-        Point3d meterComboPoint = new Point3d(
-          startingPoint.X + 8.0855 + meterComboPointXOffset,
-          startingPoint.Y - 4.2534,
-          0
-        );
-        meterComboPoints.Add(meterComboPoint);
-        if (ActiveLots.Count > 1)
-        {
-          Point3d panelBreakerPoint = new Point3d(
-            panelPoint.X + 0.1416,
-            panelPoint.Y - 0.6151 + panelBreakerPointYOffset,
-            0
-          );
-          panelBreakerPoints.Add(panelBreakerPoint);
-          Point3d intersectionPoint = new Point3d(meterComboPoint.X, panelBreakerPoint.Y, 0);
-          CadObjectFunctions.MakeLine(panelBreakerPoint, intersectionPoint);
-          CadObjectFunctions.MakeLine(intersectionPoint, meterComboPoint);
-        }
-        CadObjectFunctions.MakeText(
-          new Point3d(meterComboPoint.X + 0.2076, meterComboPoint.Y + 4.928, 0),
-          "(E)METER COMBINATION AT LOT " + ActiveLots[i].Number
-        );
-        CadObjectFunctions.MakeText(
-          new Point3d(meterComboPoint.X + 0.2076, meterComboPoint.Y + 4.7373, 0),
-          $"{ActiveLots[i].Voltage}, {ActiveLots[i].Amp}A"
-        );
-        CadObjectFunctions.MakeText(
-          new Point3d(meterComboPoint.X + 0.2076, meterComboPoint.Y + 4.5823, 0),
-          ActiveLots[i].Kaic + " KAIC"
-        );
-        CadObjectFunctions.MakeText(
-          new Point3d(meterComboPoint.X + 1.3979, meterComboPoint.Y + 2.4252, 0),
-          $"{ActiveLots[i].Amp}A/" + (ActiveLots[i].Voltage.Contains("3") ? "3P" : "2P")
-        );
-        if (i == ActiveLots.Count - 1)
-        {
-          CadObjectFunctions.MakeBlock(
-            new Point3d(meterComboPoint.X + 1.1472, meterComboPoint.Y + 1.3900, 0),
-            "IBEC 60A-2P TEMP POWER BREAKER"
-          );
-          if (ActiveLots.Count > 1)
-          {
-            CadObjectFunctions.MakeText(
-              new Point3d(meterComboPoint.X + 2.6945, meterComboPoint.Y + 0.2734, 0),
-              "(TYP.)"
-            );
-          }
-        }
-
-        meterComboPointXOffset += 3.0941;
-        panelBreakerPointYOffset -= 0.2934;
-      }
-
-      List<Point3d> batteryPoints = new List<Point3d>();
-      double batteryPointYOffset = 0;
-      for (int i = 0; i < PowerStation.NumBatts; i++)
-      {
-        batteryPoints.Add(
-          new Point3d(startingPoint.X + 6.7770, startingPoint.Y - 4.0957 + batteryPointYOffset, 0)
-        );
-        if (i < PowerStation.NumBatts - 1)
-        {
-          Point3d line1Start = new Point3d(
-            startingPoint.X + 6.7770,
-            startingPoint.Y - 4.0957 - 0.4748 + batteryPointYOffset,
-            0
-          );
-          Point3d line1End = new Point3d(line1Start.X, line1Start.Y - 0.2278, 0);
-          CadObjectFunctions.MakeLine(line1Start, line1End);
-
-          Point3d line2Start = new Point3d(line1Start.X + 0.3514, line1Start.Y, 0);
-          Point3d line2End = new Point3d(line2Start.X, line2Start.Y - 0.2278, 0);
-          CadObjectFunctions.MakeLine(line2Start, line2End);
-        }
-        batteryPointYOffset -= 0.7026;
-      }
-
-      CadObjectFunctions.MakeBlock(startingPoint, "IBEC 45KW POWER STATION");
-      panelBreakerPoints.ForEach(p => CadObjectFunctions.MakeBlock(p, "IBEC 60A-2P PANEL BREAKER"));
-      meterComboPoints.ForEach(p => CadObjectFunctions.MakeBlock(p, "IBEC METER COMBO EXISTING"));
-      batteryPoints.ForEach(p => CadObjectFunctions.MakeBlock(p, "IBEC 30KWH BATTERY BANK"));
-    }
-
-    private void MakeSingleLine30kw(Point3d startingPoint)
-    {
-      List<Lot> ActiveLots = GetActiveLots();
+      int kwId = PowerStation.KwId;
       Point3d panelPoint = new Point3d(
         startingPoint.X + 2.3873,
         startingPoint.Y
@@ -214,9 +67,41 @@ namespace GMEPSolar
           - (PowerStation.NumBatts > 4 ? 0.7026 * (PowerStation.NumBatts - 4) : 0),
         0
       );
+      string panelBlock = "IBEC 100A PANEL";
+      string wireSize = "3WG100";
+      Point3d meterComboPoint = new Point3d(startingPoint.X + 6.4373, startingPoint.Y - 4.2534, 0);
+      Point3d batteryPoint = new Point3d(startingPoint.X + 5.1288, startingPoint.Y - 4.0957, 0);
+      Point3d batteryLinePoint = new Point3d(
+        startingPoint.X + 5.1288,
+        startingPoint.Y - 4.0957 - 0.4748,
+        0
+      );
+      string powerStationBlock = "IBEC 30KW POWER STATION";
+      if (kwId == 2)
+      {
+        panelPoint = new Point3d(
+          startingPoint.X + 4.0365,
+          startingPoint.Y
+            - 6.3512
+            - (PowerStation.NumBatts > 4 ? 0.7026 * (PowerStation.NumBatts - 4) : 0),
+          0
+        );
+        panelBlock = "IBEC 200A PANEL";
+        wireSize = "3WG200";
+        meterComboPoint = new Point3d(startingPoint.X + 8.0855, startingPoint.Y - 4.2534, 0);
+        batteryPoint = new Point3d(startingPoint.X + 6.7770, startingPoint.Y - 4.0957, 0);
+        batteryLinePoint = new Point3d(
+          startingPoint.X + 6.7770,
+          startingPoint.Y - 4.0957 - 0.4748,
+          0
+        );
+        powerStationBlock = "IBEC 45KW POWER STATION";
+      }
+      List<Lot> ActiveLots = GetActiveLots();
+
       if (ActiveLots.Count > 1)
       {
-        CadObjectFunctions.MakeBlock(panelPoint, "IBEC 100A PANEL");
+        CadObjectFunctions.MakeBlock(panelPoint, panelBlock);
         Point3d upperLeft = new Point3d(panelPoint.X - 0.35, panelPoint.Y, 0);
         Point3d upperRight = new Point3d(panelPoint.X + 0.35, panelPoint.Y, 0);
         Point3d lowerLeft = new Point3d(
@@ -243,7 +128,7 @@ namespace GMEPSolar
         CadObjectFunctions.MakeLine(feederStart, panelPoint);
         CadObjectFunctions.MakeText(
           new Point3d(panelPoint.X - 0.0625, panelPoint.Y + 0.5, 0),
-          "3WG100",
+          wireSize,
           true
         );
       }
@@ -261,6 +146,7 @@ namespace GMEPSolar
         CadObjectFunctions.MakeLine(point2, point3);
         CadObjectFunctions.MakeLine(point3, point4);
       }
+
       List<Point3d> meterComboPoints = new List<Point3d>();
       List<Point3d> panelBreakerPoints = new List<Point3d>();
       double meterComboPointXOffset = 0;
@@ -268,12 +154,12 @@ namespace GMEPSolar
 
       for (int i = 0; i < ActiveLots.Count; i++)
       {
-        Point3d meterComboPoint = new Point3d(
-          startingPoint.X + 6.4373 + meterComboPointXOffset,
-          startingPoint.Y - 4.2534,
+        Point3d currentMeterComboPoint = new Point3d(
+          meterComboPoint.X + meterComboPointXOffset,
+          meterComboPoint.Y,
           0
         );
-        meterComboPoints.Add(meterComboPoint);
+        meterComboPoints.Add(currentMeterComboPoint);
         if (ActiveLots.Count > 1)
         {
           Point3d panelBreakerPoint = new Point3d(
@@ -282,36 +168,36 @@ namespace GMEPSolar
             0
           );
           panelBreakerPoints.Add(panelBreakerPoint);
-          Point3d intersectionPoint = new Point3d(meterComboPoint.X, panelBreakerPoint.Y, 0);
+          Point3d intersectionPoint = new Point3d(currentMeterComboPoint.X, panelBreakerPoint.Y, 0);
           CadObjectFunctions.MakeLine(panelBreakerPoint, intersectionPoint);
-          CadObjectFunctions.MakeLine(intersectionPoint, meterComboPoint);
+          CadObjectFunctions.MakeLine(intersectionPoint, currentMeterComboPoint);
         }
         CadObjectFunctions.MakeText(
-          new Point3d(meterComboPoint.X + 0.2076, meterComboPoint.Y + 4.928, 0),
+          new Point3d(currentMeterComboPoint.X + 0.2076, currentMeterComboPoint.Y + 4.928, 0),
           "(E)METER COMBINATION AT LOT " + ActiveLots[i].Number
         );
         CadObjectFunctions.MakeText(
-          new Point3d(meterComboPoint.X + 0.2076, meterComboPoint.Y + 4.7373, 0),
+          new Point3d(currentMeterComboPoint.X + 0.2076, currentMeterComboPoint.Y + 4.7373, 0),
           $"{ActiveLots[i].Voltage}, {ActiveLots[i].Amp}A"
         );
         CadObjectFunctions.MakeText(
-          new Point3d(meterComboPoint.X + 0.2076, meterComboPoint.Y + 4.5823, 0),
+          new Point3d(currentMeterComboPoint.X + 0.2076, currentMeterComboPoint.Y + 4.5823, 0),
           ActiveLots[i].Kaic + " KAIC"
         );
         CadObjectFunctions.MakeText(
-          new Point3d(meterComboPoint.X + 1.3979, meterComboPoint.Y + 2.4252, 0),
+          new Point3d(currentMeterComboPoint.X + 1.3979, currentMeterComboPoint.Y + 2.4252, 0),
           $"{ActiveLots[i].Amp}A/" + (ActiveLots[i].Voltage.Contains("4W") ? "3P" : "2P")
         );
         if (i == ActiveLots.Count - 1)
         {
           CadObjectFunctions.MakeBlock(
-            new Point3d(meterComboPoint.X + 1.1472, meterComboPoint.Y + 1.3900, 0),
+            new Point3d(currentMeterComboPoint.X + 1.1472, currentMeterComboPoint.Y + 1.3900, 0),
             "IBEC 60A-2P TEMP POWER BREAKER"
           );
           if (ActiveLots.Count > 1)
           {
             CadObjectFunctions.MakeText(
-              new Point3d(meterComboPoint.X + 2.6945, meterComboPoint.Y + 0.2734, 0),
+              new Point3d(currentMeterComboPoint.X + 2.6945, currentMeterComboPoint.Y + 0.2734, 0),
               "(TYP.)"
             );
           }
@@ -323,14 +209,12 @@ namespace GMEPSolar
       double batteryPointYOffset = 0;
       for (int i = 0; i < PowerStation.NumBatts; i++)
       {
-        batteryPoints.Add(
-          new Point3d(startingPoint.X + 5.1288, startingPoint.Y - 4.0957 + batteryPointYOffset, 0)
-        );
+        batteryPoints.Add(new Point3d(batteryPoint.X, batteryPoint.Y + batteryPointYOffset, 0));
         if (i < PowerStation.NumBatts - 1)
         {
           Point3d line1Start = new Point3d(
-            startingPoint.X + 5.1288,
-            startingPoint.Y - 4.0957 - 0.4748 + batteryPointYOffset,
+            batteryLinePoint.X,
+            batteryLinePoint.Y + batteryPointYOffset,
             0
           );
           Point3d line1End = new Point3d(line1Start.X, line1Start.Y - 0.2278, 0);
@@ -342,7 +226,7 @@ namespace GMEPSolar
         }
         batteryPointYOffset -= 0.7026;
       }
-      CadObjectFunctions.MakeBlock(startingPoint, "IBEC 30KW POWER STATION");
+      CadObjectFunctions.MakeBlock(startingPoint, powerStationBlock);
       panelBreakerPoints.ForEach(p => CadObjectFunctions.MakeBlock(p, "IBEC 60A-2P PANEL BREAKER"));
       meterComboPoints.ForEach(p => CadObjectFunctions.MakeBlock(p, "IBEC METER COMBO EXISTING"));
       batteryPoints.ForEach(p => CadObjectFunctions.MakeBlock(p, "IBEC 30KWH BATTERY BANK"));
@@ -386,14 +270,7 @@ namespace GMEPSolar
             return;
           }
         }
-        if (PowerStation.KwId == 1)
-        {
-          MakeSingleLine30kw(startingPoint);
-        }
-        if (PowerStation.KwId == 2)
-        {
-          MakeSingleLine45kw(startingPoint);
-        }
+        MakeSingleLine(startingPoint);
       }
       ed.SetCurrentView(currentView);
     }
